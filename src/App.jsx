8f77e2dccb47d0ba41088708480de25b1525c82a
Admin <admin@example.com>
2024-03-05 17:05:02 +0000

    updates to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const classNames = "heading title";
+  const message = "Hello World!!!";
+  const date = new Date().toDateString();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className={classNames}>{message}</div>
+      <p>Today is {date}.</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
