14d4029ee7ffd0ede0a0b3908c9ae4b625f9030d
Admin <admin@example.com>
2024-03-05 17:33:22 +0000

    added if condition

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import DateMessage from "./components/date";
 function App() {
   const classNames = "heading title";
   const message = "Hello World!!!";
+  const showDateMessage = true
   return (
     <>
       <div className={classNames}>{message}</div>
-      {/* <DateMessage></DateMessage> */}
-      {/* or */}
-      <DateMessage/>
+      {showDateMessage && <DateMessage />}
     </>
   );
 }
